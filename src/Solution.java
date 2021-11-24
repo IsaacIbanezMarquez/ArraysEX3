09439ecc6deb5c1ee22ff3e5e84f75fa5d52c7e1
Isaac Ibanez Marquez <isaacibanez00@gmail.com>
2021-11-24 22:24:28 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,25 @@
+
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int[] numeros = new int [100];
+
+        int suma = 0;
+        double media = 0;
+
+        for(int i = 0; i < numeros.length; i++)
+        {
+            numeros [i] = i + 1;
+            System.out.println(numeros[i]);
+        }
+
+        System.out.println("La suma es: " + suma);
+
+        media = (double) suma/numeros.length;
+
+        System.out.println("La media es: " + media);
+    }
+
+}
